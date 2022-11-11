479ae65a8cf4dd73f98a8a11c5c9b9f5a8d53e07
PATRICK CULLETON <pcoreyculleton@gmail.com>
2022-11-10 18:30:06 -0600

    task one finished

--- queries.sql
+++ queries.sql
@@ -1,4 +1,11 @@
 -- Part 1: Test it with SQL
+--select *
+--from INFORMATION_SCHEMA.COLUMNS
+--where TABLE_NAME='job'
+--'def', 'techjobs', 'job', 'employer', '2', NULL, 'YES', 'varchar', '255', '1020', NULL, NULL, NULL, 'utf8mb4', 'utf8mb4_0900_ai_ci', 'varchar(255)', '', '', 'select,insert,update,references', '', '', NULL
+--'def', 'techjobs', 'job', 'id', '1', NULL, 'NO', 'int', NULL, NULL, '10', '0', NULL, NULL, NULL, 'int', 'PRI', '', 'select,insert,update,references', '', '', NULL
+--'def', 'techjobs', 'job', 'name', '3', NULL, 'YES', 'varchar', '255', '1020', NULL, NULL, NULL, 'utf8mb4', 'utf8mb4_0900_ai_ci', 'varchar(255)', '', '', 'select,insert,update,references', '', '', NULL
+--'def', 'techjobs', 'job', 'skills', '4', NULL, 'YES', 'varchar', '255', '1020', NULL, NULL, NULL, 'utf8mb4', 'utf8mb4_0900_ai_ci', 'varchar(255)', '', '', 'select,insert,update,references', '', '', NULL
 
 -- Part 2: Test it with SQL
 
