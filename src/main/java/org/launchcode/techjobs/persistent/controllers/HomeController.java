8b86b985ade75e88526c209b8c0059b4bcaf4bb8
PATRICK CULLETON <pcoreyculleton@gmail.com>
2022-11-14 20:53:03 -0600

    task 3 done

--- src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
@@ -2,7 +2,10 @@ package org.launchcode.techjobs.persistent.controllers;
 
 import org.launchcode.techjobs.persistent.models.Employer;
 import org.launchcode.techjobs.persistent.models.Job;
+import org.launchcode.techjobs.persistent.models.Skill;
 import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
+import org.launchcode.techjobs.persistent.models.data.JobRepository;
+import org.launchcode.techjobs.persistent.models.data.SkillRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,6 +23,11 @@ import java.util.Optional;
 public class HomeController {
     @Autowired
     private EmployerRepository employerRepository;
+    @Autowired
+    private SkillRepository skillRepository;
+    @Autowired
+    private JobRepository jobRepository;
+
     @RequestMapping("")
     public String index(Model model) {
 
@@ -31,7 +39,8 @@ public class HomeController {
     @GetMapping("add")
     public String displayAddJobForm(Model model) {
         model.addAttribute("title", "Add Job");
-        model.addAttribute("employerId", employerRepository.findAll());
+        model.addAttribute("employers", employerRepository.findAll());
+        model.addAttribute("skills", skillRepository.findAll());
         model.addAttribute(new Job());
         return "add";
     }
@@ -49,14 +58,20 @@ public class HomeController {
             Employer newEmployer = (Employer) optEmployers.get();
             newJob.setEmployer(newEmployer);
         }
+        List<Skill>optSkills = (List<Skill>) skillRepository.findAllById(skills);
+        newJob.setSkills(optSkills);
+        jobRepository.save(newJob);
         return "redirect:";
     }
 
     @GetMapping("view/{jobId}")
     public String displayViewJob(Model model, @PathVariable int jobId) {
-
+        Optional optJobId = jobRepository.findById(jobId);
+        if (optJobId.isPresent()) {
+            Job newJob = (Job) optJobId.get();
+            model.addAttribute("job", newJob);
         return "view";
-    }
-
-
-}
+        }else {
+        return "redirect:";
+        }
+}}
